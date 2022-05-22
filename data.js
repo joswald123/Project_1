3af4024a1899e02bc2c7f18d37fed1645d72c29b
joswald123 <julianaandreaoswald@gmail.com>
2022-05-21 21:23:30 -0700

    Added project

---
+++ data.js
@@ -0,0 +1,40 @@
+const quotes = [
+    {
+        quote: "What necessity is there to dwell on the Past, when the Present is so much surer—the Future so much brighter?",
+        source: " ~Charlotte Bronte",
+        citation: "Jane Eyre",
+        year: 1847
+    },
+    {
+        quote: "What necessity is there to dwell on the Past, when the Present is so much surer—the Future so much brighter?",
+        source: " ~Jane Eyre",
+        citation: "(Charlotte Bronte, 1847, p. 265)",
+        year: 1848
+    },
+    {
+        quote: "What necessity is there to dwell on the Past, when the Present is so much surer—the Future so much brighter?",
+        source: " ~Jane Eyre",
+        citation: "(Charlotte Bronte, 1c847, p. 265)",
+        year: 1849
+    },
+    {
+        quote: "What necessity is there to dwell on the Past, when the Present is so much surer—the Future so much brighter?",
+        source: " ~Jane Eyre",
+        citation: "(Charlotte Bronte, 1847, p. 265)",
+        year: 1850
+    },
+    {
+        quote: "What necessity is there to dwell on the Past, when the Present is so much surer—the Future so much brighter?",
+        source: " ~Jane Eyre",
+        citation: "(Charlotte Bronte, 1847, p. 265)",
+        year: 1851
+    },
+    {
+        quote: "What necessity is there to dwell on the Past, when the Present is so much surer—the Future so much brighter?",
+        source: " ~Jane Eyre",
+        citation: "(Charlotte Bronte, 1847, p. 265)",
+        year: 1852
+    }
+];
+
+console.log(quotes);
